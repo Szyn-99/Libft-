151578ef5b01da74d6d1ab600f2abc6190931ae2
Aymane El Halkouj <aymel-ha@e1r1p2.1337.ma>
2025-12-22 01:16:47 +0100

    script

--- pucher.sh
+++ pucher.sh
@@ -2,17 +2,45 @@
 
 set -euo
 
-read -p "Enter commit message: " COMMIT_MSG
+read -ep $'Enter Operation:\n(1) for cloning\n(2) for pushing\n(3) Cloning and Pushing a file/directory\n|x| Operation : ' OP
+if [ "$OP" == '1' ];then
+    read -p "Enter Repository Link: " REPO_LINK
+    read -p "Enter Repository Name: " REPO_NAME
+    git clone "$REPO_LINK" "$REPO_NAME"
+    cd "$REPO_NAME"
+    clear
+    echo -e "Success\n"
+    exit 0
+fi
 
-# read -p "Enter repository link : " REPO_LINK
+if [ "$OP" == '2' ];then
+    read -p "Enter commit message: " COMMIT_MSG
+    git add .
+    git status
+    git commit -m "$COMMIT_MSG"
+    git push 
+    clear
+    echo -e "Success\n"
+    exit 0
+fi
+
+if [ "$OP" == '3' ];then
+    read -p "Enter Repository Link: " REPO_LINK
+    read -p "Enter Repository Name: " REPO_NAME
+    read -p "Enter file/directory path/name: " fd
+    git clone "$REPO_LINK" "$REPO_NAME"
+    cd "$REPO_NAME"
+    cp -r "$fd" .
+    read -p "Enter commit message: " COMMIT_MSG
+    git add .
+    git status
+    git commit -m "$COMMIT_MSG"
+    git push
+    clear
+    echo -e "Success\n"
+    exit 0
+fi
 
 
-git add .
-
-git status
-
-git commit -m "$COMMIT_MSG"
-
-git push 
-
-
+echo -e "Please Enter Valid Args"
+exit 1
